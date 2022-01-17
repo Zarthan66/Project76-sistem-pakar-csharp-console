ae3abd50d5142e40a4c0d6be21e3ff86dc73ae60
Zarthan <46505448+Zarthan66@users.noreply.github.com>
2022-01-17 22:23:45 +0700

    Improve text output

--- Sistem Pakar CSharp Console/Program.cs
+++ Sistem Pakar CSharp Console/Program.cs
@@ -129,7 +129,7 @@ else if(answer == "1,2,3,4,5,6,7,8,9,10,11,12")
 }
 else
 {
-    Console.Write("Penyakit tidak dapat teridentifikasi berdasarkan data yang ada");
+    Console.WriteLine("Penyakit tidak dapat teridentifikasi berdasarkan data yang ada.");
     Console.WriteLine("Pastikan Input mengikuti format sebagai berikut");
     Console.WriteLine("Format: g1,g2,g3,g4,dst...");
     Console.WriteLine("Contoh: 9,17,18,20,23,24");
